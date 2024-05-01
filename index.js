1c5ae7a83001a6d28e3808d80e43295cceba17d8
DeeBest <simphiwedladla8@gmail.com>
2024-05-01 22:15:28 +0200

    added a function to dynamically update the clock

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ const month = new Date().toLocaleString('en', { month: 'long' }).slice(0, 3);
 
 const dateDisplay = document.getElementById('date-display');
 dateDisplay.textContent = `${day}/${month}/${currentYear}`;
+
+const hoursHand = document.getElementById('hours-hand');
+const minutesHand = document.getElementById('minutes-hand');
+const secondsHand = document.getElementById('seconds-hand');
+
+updateClock();
+
+function updateClock() {
+  const hour = new Date().getHours();
+  const minute = new Date().getMinutes();
+  const second = new Date().getSeconds();
+
+  const hourDegree = (hour / 12) * 360;
+  const minuteDegree = (minute / 60) * 360;
+  const secondDegree = (second / 60) * 360;
+
+  secondsHand.style.transform = `rotate(${secondDegree}deg)`;
+  minutesHand.style.transform = `rotate(${minuteDegree}deg)`;
+  hoursHand.style.transform = `rotate(${hourDegree}deg)`;
+
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
