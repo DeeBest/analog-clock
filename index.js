90f1ec5023584698691c9086a7845ca9a842300f
DeeBest <simphiwedladla8@gmail.com>
2024-05-01 21:37:01 +0200

    applied styling

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
+
+const day = new Date().getDate().toString().padStart(2, '0');
+const month = new Date().toLocaleString('en', { month: 'long' }).slice(0, 3);
+
+const dateDisplay = document.getElementById('date-display');
+dateDisplay.textContent = `${day}/${month}/${currentYear}`;
